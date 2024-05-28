8eec8718a0f77223cfe7a13beb7e5372e4b447a2
Pegasus <nwillson2601@gmail.com>
2024-05-28 08:09:37 +0300

    test

--- app/main.ts
+++ app/main.ts
@@ -15,12 +15,6 @@ const createHttpResponse = (startLine: string, headers?: string[], body?: string
 const getStringByteLength = (string: string | undefined) => {
     return Buffer.byteLength(string || '','utf-8')
 }
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-// Uncomment this to pass the first stage
-server.listen(4221, 'localhost', () => {
-    console.log('Server is running on port 4221');
-});
 server.on('connection', (socket) => {
     socket.on('data', (dataBuf) => {
         const content = dataBuf.toString('utf-8');
