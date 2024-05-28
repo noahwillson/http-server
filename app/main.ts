ad7bbcdda55b590d285e1e70344e85d433ab0d75
Pegasus <nwillson2601@gmail.com>
2024-05-28 05:07:53 +0300

    test

--- app/main.ts
+++ app/main.ts
@@ -1,8 +1,7 @@
 import * as net from 'net';
 
 const server = net.createServer((socket) => {
-    socket.write("HTTP/1.1 200 OK\r\n\r\n
-    ")
+    socket.write("HTTP/1.1 200 OK\r\n\r\n");
     socket.end();
 });
 
