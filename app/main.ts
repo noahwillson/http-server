0957530f78a0ce47a614e6b1c8c1d2ee6b349995
Pegasus <nwillson2601@gmail.com>
2024-05-28 06:11:47 +0300

    test

--- app/main.ts
+++ app/main.ts
@@ -2,20 +2,28 @@ import * as net from 'net';
 
 const server = net.createServer((socket: { write: (arg0: string) => void; on: (arg0: string, arg1: (data: any) => void) => void; end: () => void; }) => {
     socket.on("data", (data: { toString: () => any; }) => {
-        const userAgent = data.toString().split("\r\n")[0].split(" ")
         const req = data.toString()
-        const path = req.split(" ")[1];
+        const [requestLine, ...headerLines] = req.split("\r\n");
+        const path = requestLine.split(" ")[1];
+        const headers = headerLines.reduce((acc: { [x: string]: any; }, line: { split: (arg0: string) => [any, any]; }) => {
+            const [key,value] = line.split(": ")
+            if(key && value) {
+                acc[key] = value
+            }
+            return acc;
+        },{} as Record<string,string>)
         let res;
         if(path === "/"){
             const body = "Welcome to root"
             res = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${body.length}\r\n\r\n${body}`;
-        }else if(path === "/user-agent"){
-            const userAgentValue = userAgent.split(": ")[1]
-            res = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-length: ${userAgentValue.Length}\r\n\r\n${userAgentValue}`
         }else if(path.startsWith("/echo/")){
             const echoStr = path.slice(6);
             res = `HTTP/1.1 200 OK\r\n\Content-Type: text/plain\r\nContent-length: ${echoStr.length}\r\n\r\n${echoStr}`;
-        }else {
+        }else if (path === "/user-agent") {
+            const userAgent = headers["User-Agent"] || "Unknown";
+            res = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
+        }
+        else {
             res = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\n"
         }
         socket.write(res);
